d0ac19429915c4b09245ebfbcacc52ff3f9f2415
rumelia <mrahman@iwu.edu>
2020-03-01 14:54:25 +0000

    Add virus_count_checker thread

--- mrahman_cs456_a3.c
+++ mrahman_cs456_a3.c
@@ -11,6 +11,11 @@
 // mapping: index n -> virus count for fn
 int virus_count_array[4];
 
+void * check_virus_count(void *arg)
+{
+
+}
+
 void * neutralize_viruses(void *arg)
 {
 
@@ -19,6 +24,7 @@ void * neutralize_viruses(void *arg)
 void * let_in_viruses(void  *arg)
 {
 
+
 }
 
 int main (int argc, char **argv)
@@ -26,36 +32,43 @@ int main (int argc, char **argv)
   // declare me and friend threads
   pthread_t me;
   pthread_t f0, f1, f2, f3;
+  pthread_t virus_count_checker;
 
   // assign me thread ----------------------------------
   if (pthread_create(&me, NULL, &neutralize_viruses, NULL))
   {
-    printf("Could not created ME thread\n");
+    printf("Could not create ME thread\n");
     return -1;
   }
 
   // assign friend threads ------------------------------
   if (pthread_create(&f0, NULL, &let_in_viruses, NULL))
   {
-    printf("Could not created F0 thread\n");
+    printf("Could not create F0 thread\n");
     return -1;
   }
 
   if (pthread_create(&f1, NULL, &let_in_viruses, NULL))
   {
-    printf("Could not created F1 thread\n");
+    printf("Could not create F1 thread\n");
     return -1;
   }
 
   if (pthread_create(&f2, NULL, &let_in_viruses, NULL))
   {
-    printf("Could not created F2 thread\n");
+    printf("Could not create F2 thread\n");
     return -1;
   }
 
   if (pthread_create(&f3, NULL, &let_in_viruses, NULL))
   {
-    printf("Could not created F3 thread\n");
+    printf("Could not create F3 thread\n");
+    return -1;
+  }
+
+  if (pthread_create(&virus_count_checker, NULL, &check_virus_count, NULL))
+  {
+    printf("Could not create CHECKER thread\n");
     return -1;
   }
 
